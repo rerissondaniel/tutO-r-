81e4d195cbb0f0ea28690513fd88ec9bc3fee601
Raoni Smaneoto <raoni.smaneoto@ccc.ufcg.edu.br>
2018-05-16 23:18:30 -0300

    Wrapping firebase's confs into a file

---
+++ frontend/src/firebase-config.js
@@ -0,0 +1,8 @@
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAJUlrMFgopCjG9ttrOJVe5bPKzzSTJeqM",
+  authDomain: "tuto-r-11ad6.firebaseapp.com",
+  databaseURL: "https://tuto-r-11ad6.firebaseio.com",
+  projectId: "tuto-r-11ad6",
+  storageBucket: "tuto-r-11ad6.appspot.com",
+  messagingSenderId: "518529534195"
+};
