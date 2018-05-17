608267c06e94259a0a3eb3dc7431434e2800bc0b
Raoni Smaneoto <raoni.smaneoto@ccc.ufcg.edu.br>
2018-05-16 23:13:40 -0300

    Setting up firebase and authentication

---
+++ frontend/src/services/AuthService.js
@@ -0,0 +1,40 @@
+import _ from "lodash";
+
+const authObj = firebase.auth();
+
+var userInfo = {};
+
+export const login = function login(email, password) {
+  authObj.signInWithEmailAndPassword(email, password).then(function success(user) {
+    configUser(user);
+  }).catch(function error(error) {
+    console.error(error);
+  });
+};
+
+export const createUser = function createUser(email, password) {
+  authObj.createUserWithEmailAndPassword(email, password).then(function success(user) {
+    configUser(user);
+  }).catch(function error(error) {
+    console.error(error);
+  });
+};
+
+export const logout = function logout() {
+  authObj.signOut().then(() => {
+    delete window.localStorage.userInfo;
+  });
+};
+
+export const getCurrentUser = function getCurrentUser() {
+  return authObj.currentUser;
+};
+
+export const configUser = function configUser(user) {
+  _.extend(userInfo, user);
+  saveUser();
+};
+
+function saveUser() {
+  window.localStorage.userInfo = JSON.stringify(userInfo);
+}
