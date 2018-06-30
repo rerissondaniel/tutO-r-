da943e798968d924d6a0d3bb62c6eee1c4c95232
lucasaarcoverde <lucas.arcoverde.nascimento@ccc.ufcg.edu.br>
2018-06-30 19:50:16 -0300

    Adding profile screen

---
+++ frontend/src/components/Profile/Profile.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import BadgesCard from './BadgesCard';
+import ProfileCard from './ProfileCard';
+import './Profile.css';
+
+export default class MainPage extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+ 	render() {
+	   return (
+	   	<div className="flex fr">
+        <ProfileCard name="Gustavo Ribeiro" city="Campina Grande" institution="UFCG" username="@tapioca" codeforcesUser="BigHeadOfUsAll"/>
+		   	<BadgesCard />
+		  </div>
+	   );
+	 }
+}
