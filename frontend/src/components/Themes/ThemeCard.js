f4a0ec70f15ed8b92f88b52a606464a2cffa34d0
lucasaarcoverde <lucas.arcoverde.nascimento@ccc.ufcg.edu.br>
2018-07-01 14:15:35 -0300

    Fixing indentation

--- frontend/src/components/Themes/ThemeCard.js
+++ frontend/src/components/Themes/ThemeCard.js
@@ -11,20 +11,20 @@ export default class ThemeCard extends Component {
     super(props);
   };
 
- 	render() {
- 		let {theme, requisites, status} = this.props;
-	   return (
-	   	<div>
+  render() {
+	  let {theme, requisites, status} = this.props;
+    return (
+ 	    <div>
         <article className="mr3 mt2 mb2 br3 ba w-96 b--dark-blue dib">
-        	<div className="fl">
-        		<p className="f3 ml2 b green">{theme}</p>
-        		<p className="f5 ml2 mt4 b"> Pre-requisites: {requisites}</p>
-        	</div>
-        	<article className="br3 ma3 mt4 ba b--dark-blue fr mb3" style={{backgroundColor: bgColors[status], borderColor: bgColors[status]}}>
-        		<p className="w4 f4 center tc white">{status}</p>
-        	</article>
+    	    <div className="fl">
+  		      <p className="f3 ml2 b green">{theme}</p>
+  		      <p className="f5 ml2 mt4 b"> Pre-requisites: {requisites}</p>
+  	      </div>
+    	    <article className="br3 ma3 mt4 ba b--dark-blue fr mb3" style={{backgroundColor: bgColors[status], borderColor: bgColors[status]}}>
+    		    <p className="w4 f4 center tc white">{status}</p>
+  	      </article>
         </article>
-		  </div>
-	   );
-	 }
+      </div>
+    );
+  }
 }
--- frontend/src/components/Themes/Themes.js
+++ frontend/src/components/Themes/Themes.js
@@ -5,21 +5,21 @@ import './Themes.css';
 export default class Themes extends Component {
   constructor(props) {
     super(props);
-  }
+  };
 
  	render() {
-	   return (
-	   	<div>
-	   		<div className="ml3">
+    return (
+ 	    <div>
+ 		    <div className="ml3">
           <p className="f2 blue mb0">Themes</p>
-	   		</div>
-		   	<div className="vh-75 w-70 h6 pre ma3">
-	        <ThemeCard theme="Dynamic Programming" requisites="Bitmasks, Backtracking" status="Explorado"/>
-	        <ThemeCard theme="Bitmasks" requisites="Number Representation, Numeric Bases" status="Explorando"/>
-	        <ThemeCard theme="Brute force" requisites="None" status="Explorar"/>
-	        <ThemeCard theme="Greedy" requisites="None" status="Explorar"/>
-			  </div>
-      </div>
-	   );
+ 		    </div>
+   	      <div className="vh-75 w-70 h6 pre ma3">
+            <ThemeCard theme="Dynamic Programming" requisites="Bitmasks, Backtracking" status="Explorado"/>
+            <ThemeCard theme="Bitmasks" requisites="Number Representation, Numeric Bases" status="Explorando"/>
+            <ThemeCard theme="Brute force" requisites="None" status="Explorar"/>
+            <ThemeCard theme="Greedy" requisites="None" status="Explorar"/>
+	      </div>
+  </div>
+ );
 	 }
 }
