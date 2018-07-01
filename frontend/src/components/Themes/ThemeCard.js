07fb89d92452ec27405eb6003cdfa0f9b4757bf2
lucasaarcoverde <lucas.arcoverde.nascimento@ccc.ufcg.edu.br>
2018-07-01 14:04:06 -0300

    Adding Themes Screen

---
+++ frontend/src/components/Themes/ThemeCard.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+
+const bgColors = {
+  "Explorar": "#00BFFF",
+  "Explorando": "#FFD700",
+  "Explorado": "#32CD32"
+};
+
+export default class ThemeCard extends Component {
+  constructor(props) {
+    super(props);
+  };
+
+ 	render() {
+ 		let {theme, requisites, status} = this.props;
+	   return (
+	   	<div>
+        <article className="mr3 mt2 mb2 br3 ba w-96 b--dark-blue dib">
+        	<div className="fl">
+        		<p className="f3 ml2 b green">{theme}</p>
+        		<p className="f5 ml2 mt4 b"> Pre-requisites: {requisites}</p>
+        	</div>
+        	<article className="br3 ma3 mt4 ba b--dark-blue fr mb3" style={{backgroundColor: bgColors[status], borderColor: bgColors[status]}}>
+        		<p className="w4 f4 center tc white">{status}</p>
+        	</article>
+        </article>
+		  </div>
+	   );
+	 }
+}
---
+++ frontend/src/components/Themes/Themes.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import ThemeCard from './ThemeCard';
+import './Themes.css';
+
+export default class Themes extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+ 	render() {
+	   return (
+	   	<div>
+	   		<div className="ml3">
+          <p className="f2 blue mb0">Themes</p>
+	   		</div>
+		   	<div className="vh-75 w-70 h6 pre ma3">
+	        <ThemeCard theme="Dynamic Programming" requisites="Bitmasks, Backtracking" status="Explorado"/>
+	        <ThemeCard theme="Bitmasks" requisites="Number Representation, Numeric Bases" status="Explorando"/>
+	        <ThemeCard theme="Brute force" requisites="None" status="Explorar"/>
+	        <ThemeCard theme="Greedy" requisites="None" status="Explorar"/>
+			  </div>
+      </div>
+	   );
+	 }
+}
