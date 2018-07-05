5c6dcc7bf0a2f75a6fb653d59ab3c5016c435893
gugabribeiro <gustavo.ribeiro@ccc.ufcg.edu.br>
2018-07-04 22:46:51 -0300

    #18 Change component name from LanguageSelector to LanguageSelectorItem

---
+++ frontend/src/components/Notifications.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Notifications extends Component {
+  render() {
+    
+  }
+}
+
+export default Notification;
