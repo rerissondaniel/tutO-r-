9fa14daae577b30d0abc6ff93206598c0be313ab
lucasArcoverde <lucas.arcoverde.nascimento@ccc.ufcg.edu.br>
2018-07-05 13:10:30 -0300

    Trying to fix indentation

--- frontend/src/components/Profile/Profile.js
+++ frontend/src/components/Profile/Profile.js
@@ -4,16 +4,16 @@ import ProfileCard from './ProfileCard';
 import './Profile.css';
 
 export default class Profile extends Component {
-  constructor(props) {
-    super(props);
-  };
+	constructor(props) {
+		super(props);
+	};
 
- 	render() {
-	   return (
-	   	<div className="flex fr">
-        <ProfileCard name="Gustavo Ribeiro" city="Campina Grande" institution="UFCG" username="@tapioca" codeforcesUser="BigHeadOfUsAll"/>
-		   	<BadgesCard />
-		  </div>
-	   );
-	 };
+	render() {
+		return (
+			<div className="flex fr">
+				<ProfileCard name="Gustavo Ribeiro" city="Campina Grande" institution="UFCG" username="@tapioca" codeforcesUser="BigHeadOfUsAll"/>
+				<BadgesCard />
+			</div>
+		);
+	};
 }
