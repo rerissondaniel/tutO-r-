84760bb210e937b18139f69bfb512f76ea4dd8f6
lucasArcoverde <lucas.arcoverde.nascimento@ccc.ufcg.edu.br>
2018-07-05 13:22:51 -0300

    Trying to fix indentation

--- frontend/src/components/Profile/BadgesCard.js
+++ frontend/src/components/Profile/BadgesCard.js
@@ -1,1 +1,21 @@
-import React, {Component} from 'react';import Badge from './Badge';export default class BadgesCard extends Component {  constructor(props) {    super(props);  }; 	render() {	  return (	    <article className="vh-75 ma3 ml1 w-50">	    	<p className="f2 blue ml3 mb0">Badges</p>	    	<div className="vh-60 ma2 w-98 ba b--dark-blue">		      <Badge bgColor="blue" contentText="> 1000 on Codeforces"/>		      <Badge bgColor="gold" contentText="Dynamic Programming Master"/>		      <Badge bgColor="green" contentText="Added a Theme"/>	    	</div>	    </article>	  );  };}
+import React, {Component} from 'react';
+import Badge from './Badge';
+
+export default class BadgesCard extends Component {
+  constructor(props) {
+    super(props);
+  };
+
+ 	render() {
+	  return (
+	    <article className="vh-75 ma3 ml1 w-50">
+	    	<p className="f2 blue ml3 mb0">Badges</p>
+	    	<div className="vh-60 ma2 w-98 ba b--dark-blue">
+		      <Badge bgColor="blue" contentText="> 1000 on Codeforces"/>
+		      <Badge bgColor="gold" contentText="Dynamic Programming Master"/>
+		      <Badge bgColor="green" contentText="Added a Theme"/>
+	    	</div>
+	    </article>
+	  );
+  };
+}
