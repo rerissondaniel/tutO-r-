74a6a15366a287267341d8086346b13e0a87b84e
Rerisson Daniel <rerissondaniel@gmail.com>
2018-07-13 19:48:44 -0300

    Add user interceptor

--- backend/src/routes/api/user.route.js
+++ backend/src/routes/api/user.route.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const userCtrl = require('../../controllers/user.controller');
+const interceptor = require('../../controllers/interceptors/user.interceptor');
 
 router.post('', userCtrl.create);
-router.get('', userCtrl.get);
+router.get('', interceptor.authenticate, userCtrl.get);
 
 module.exports = router;
