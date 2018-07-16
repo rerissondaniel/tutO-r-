c8ff0e8dc89e74b3dd4d7c5c5ded26b52c3931cb
lucasArcoverde <lucas.arcoverde.nascimento@ccc.ufcg.edu.br>
2018-07-16 11:35:17 -0300

    Adding pull requests changes

--- frontend/src/components/Profile/Badge.js
+++ frontend/src/components/Profile/Badge.js
@@ -6,8 +6,8 @@ export default class Badge extends Component {
   };
 
  	render() {
- 		let {bgColor, contentText} = this.props;
- 		let badgeClass = `h-10 dib w5 ma3 br3 ba b--black-10 bg-${bgColor}`;
+ 		const {bgColor, contentText} = this.props;
+ 		const badgeClass = `h-10 dib w5 ma3 br3 ba b--black-10 bg-${bgColor}`;
 	  return (
 	    <article className={badgeClass}>
 	      <p className="tc center">{contentText}</p>
