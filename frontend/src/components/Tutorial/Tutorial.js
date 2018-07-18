d9675d9db7ca1d40373a4db098818565cb79f2ce
lucasaarcoverde <lucas.arcoverde.nascimento@ccc.ufcg.edu.br>
2018-07-18 09:13:57 -0300

    Adding tutorial screen

---
+++ frontend/src/components/Tutorial/Tutorial.js
@@ -0,0 +1,59 @@
+import React, {Component} from 'react';
+
+const problems = [
+  "http://codeforces.com/problemset/problem/1009/G",
+  "http://codeforces.com/problemset/problem/1009/D",
+  "http://codeforces.com/problemset/problem/1009/B"
+];
+
+const references = [
+  "https://www.geeksforgeeks.org/dynamic-programming/",
+  "https://www.hackerearth.com/practice/algorithms/dynamic-programming/introduction-to-dynamic-programming-1/tutorial/"
+];
+class Tutorial extends Component {
+  constructor(props) {
+  	super(props);
+  };
+
+  render() {
+  	return (
+  		<div className="w-70 fr ma4">
+  			<p className="green f2 mb1">Dynamic Programming</p>
+  			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  			<p className="blue f2 mb1">Problems</p>
+  			{
+  				problems.map((problem, i) => {
+  					return (
+  						<div>
+  							<a href={problem}>- {problem}</a>
+  						</div>
+  					);
+  				})
+  			}
+  			<p className="blue f2 mb1">References</p>
+  			{
+	  			references.map((reference, i) => {
+	  					return (
+	  						<div>
+	  							<a href={reference}>- {reference}</a>
+	  						</div>
+	  					);
+	  				})
+	  		}
+  		</div>
+  	);
+  };
+}
+
+export default Tutorial;
